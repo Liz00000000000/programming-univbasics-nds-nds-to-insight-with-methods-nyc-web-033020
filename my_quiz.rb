ff55cdf54d7d5e218e66f06b77bfbed58b19532e
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-29 17:52:07 +0000

    Automatically backed up by Learn

--- my_quiz.rb
+++ my_quiz.rb
@@ -7,7 +7,7 @@ end
 
 p1 = "How cute is Greggory?\n a)Hes ok\n b)Eh\n c)Cutest boy in the wolrd"
 p2 = "What color is Greggory?\n a)Orange duh\n b)Black cat, spooky!\n c)I dont remember off the top of my head"
-p3 = "Who is Greggory?\n a)Celeberty!\n b)Your bf\n c)An unforgettable creature who happens to be a cat"
+p3 = "Who is Greggory?\n a)Celeberty!\n b)Your bf\n c)An unforgettable creature"
 
 questions = [
   Question.new(p1, "c")
